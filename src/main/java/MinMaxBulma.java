e0412dbc111ca58987a0a06b39dcef17ccddd402
Beyza Karaman <beyzak@posta.mu.edu.tr>
2025-03-07 15:52:32 +0300

    m

--- src/main/java/MinMaxBulma.java
+++ src/main/java/MinMaxBulma.java
@@ -4,6 +4,30 @@ public class MinMaxBulma {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        
+
+        System.out.print("Kaç tane sayı gireceksiniz : ");
+        int n = scanner.nextInt();
+
+
+        System.out.println("1. sayıyı girin : ");
+        int sayi = scanner.nextInt();
+        int min = sayi;
+        int max = sayi;
+
+
+        for (int i = 2; i<=n;i++){ //Kaç tane sayı girdiğine bağlı olarak dönecek
+            System.out.println(i + ". sayıyı giriniz");
+            sayi = scanner.nextInt();
+
+            if (sayi < min)  min = sayi;
+
+            if (sayi > max)  max = sayi;
+
+        }
+        System.out.println("En küçük sayı : " + min);
+        System.out.println("En büyük sayı : " + max);
+
+        scanner.close();
+
     }
 }
